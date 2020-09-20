5b9923945611656b09905ef51050370ebdb503d3
christinatrapp <70817568+christinatrapp@users.noreply.github.com>
2020-09-20 14:48:48 -0500

    Javascript intro, console text

---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log("Hello! Welcome to my portfolio site.")
+
+
+//this is how to leave a comment in JS//
+
+// Question: What does it mean to "print a line" to a browser's console with console.log? //
