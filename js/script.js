ed7025cf0f51e43afbfda0988779c4dfc07256d1
christinatrapp <70817568+christinatrapp@users.noreply.github.com>
2020-10-05 20:16:07 -0500

    Coding with JS

--- js/script.js
+++ js/script.js
@@ -3,4 +3,4 @@ console.log("Hello! Welcome to my portfolio site.")
 
 //this is how to leave a comment in JS//
 
-// Question: What does it mean to "print a line" to a browser's console with console.log? //
+// Question: What does it mean to "print a line" to a browser's console with console.log? Is this just for developers or used by anyone else? //
