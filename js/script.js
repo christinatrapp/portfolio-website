fc04ce31a1eb93ef07a6aa9dc9b2a7aa67a65ee4
christinatrapp <70817568+christinatrapp@users.noreply.github.com>
2020-10-10 22:21:47 -0500

    JS Lesson 2.3 Updates

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 console.log("Hello! Welcome to my portfolio site.")
 
-
-//this is how to leave a comment in JS//
-
-// Question: What does it mean to "print a line" to a browser's console with console.log? Is this just for developers or used by anyone else? //
+// JS Lesson 2.3 //
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
